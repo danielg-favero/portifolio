b0bb0a8150dfb83780934e964ed19c01b076b144
Daniel Gustavo Favero <danielfavero17@gmail.com>
2023-07-21 10:41:37 -0300

    styles: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 export const Footer: React.FC = () => {
   return (
-    <div>
-      <p>Copyright © 2022 - Daniel Gustavo Favero</p>
+    <div className="flex justify-center w-full p-8 bg-primary-95">
+      <p className="text-sm font-light text-neutral-100">
+        Copyright © 2022 - Daniel Gustavo Favero
+      </p>
     </div>
   );
 };
