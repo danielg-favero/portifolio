067c0708e02c28a4e75a8a8decfcf82aa98452ca
Daniel Gustavo Favero <danielfavero17@gmail.com>
2025-11-11 23:37:37 -0300

    fix: midia

--- src/slices/OnTv/index.tsx
+++ src/slices/OnTv/index.tsx
@@ -22,26 +22,26 @@ const OnTv = ({ slice }: OnTvProps): JSX.Element => {
   return (
     <Section>
       <SectionTitle id="na-midia">Na Midia</SectionTitle>
-      <div className="flex gap-8 flex-wrap ">
+      <div className="flex gap-16 gap-10 flex-wrap ">
         {slice.items.map((item, index) => (
           <FadeIn key={index}>
             <PrismicLink field={item.link}>
               <div className="w-full lg:w-[400px] h-full max-w-md flex flex-col bg-primary-95 hover:scale-105 transition-all active:scale-95 active:opacity-75">
                 <Image
                   field={item.thumbnail}
-                  className="rounded-t-lg object-cover w-full max-w-md lg:w-[400px] h-[200px]"
+                  className="object-cover aspect-video"
                 />
                 <div
                   style={{ height: "inherit" }}
-                  className="bg-primary-80 p-3 lg:p-4 rounded-b-lg flex flex-col gap-2"
+                  className="bg-primary-80 p-5 lg:p-6 flex flex-col gap-4"
                 >
+                  <RichText
+                    field={item.post_title}
+                    className="font-medium text-xs lg:!text-base break-words"
+                  />
                   <Badge size="sm" variant="secondary">
                     {item.post_type}
                   </Badge>
-                  <RichText
-                    field={item.post_title}
-                    className="font-medium text-xs lg:!text-base"
-                  />
                 </div>
               </div>
             </PrismicLink>
