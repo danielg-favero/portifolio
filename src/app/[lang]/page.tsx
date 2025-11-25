835fb32829dac69885ca35de85ed50e254aa0ac1
Daniel Gustavo Favero <danielfavero17@gmail.com>
2025-11-25 11:13:37 -0300

    fix: adiciona cache para documentos do prismic

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,20 +1,31 @@
+import { cache } from "react";
+
 import { Header } from "@/components";
 import { LanguageSwitcher } from "@/components/LanguageSwitcher";
 import { repositoryName } from "@/prismicio";
 import { components } from "@/slices";
-import { Locales } from "@/types";
+import { AvailableLocales, Locales } from "@/types";
 import { getLocales } from "@/utils";
 import { createClient } from "@prismicio/client";
 import { SliceZone } from "@prismicio/react";
 
+const client = createClient(repositoryName);
+
+const getHome = (lang: AvailableLocales) =>
+  cache(async () => await client.getSingle("homepage", { lang }));
+const getHeader = (lang: AvailableLocales) =>
+  cache(async () => await client.getSingle("page_header", { lang }));
+
 export default async function Home({
   params: { lang },
 }: {
-  params: { lang: string };
+  params: { lang: AvailableLocales };
 }) {
-  const client = createClient(repositoryName);
-  const home = await client.getSingle("homepage", { lang });
-  const header = await client.getSingle("page_header", { lang });
+  const getLocatedHome = getHome(lang);
+  const getLocatedHeader = getHeader(lang);
+
+  const home = await getLocatedHome();
+  const header = await getLocatedHeader();
 
   const locales = (await getLocales(home, client)) as Locales[];
 
